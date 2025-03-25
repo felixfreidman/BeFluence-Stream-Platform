89cf3b798f91e04a27264ab874fde5e74f8a0b8e
Felix Freidman <felixfreidman@gmail.com>
2025-03-25 14:14:17 +0300

    header component done

--- build/assets/js/main.js
+++ build/assets/js/main.js
@@ -76,3 +76,39 @@ function deleteWarningSpans() {
   });
 }
 deleteWarningSpans();
+var wrapperZoom = document.querySelector('.wrapperZoom');
+var initZoom = 1;
+function reportWindowSize() {
+  var currentWidth = window.innerWidth;
+  var initWidth = 0;
+  var zoomLevelPerPixel = 0;
+  var zoomLevel = 0;
+  if (currentWidth > 1399) {
+    initWidth = 1440;
+    zoomLevelPerPixel = 0.0005;
+    calcZoom(initZoom, initWidth, currentWidth, zoomLevelPerPixel);
+  }
+  if (currentWidth <= 1399 && currentWidth >= 961) {
+    zoomLevelPerPixel = 0.0007;
+    initWidth = 1440;
+    calcZoom(initZoom, initWidth, currentWidth, zoomLevelPerPixel);
+  }
+  if (currentWidth <= 960 && currentWidth > 639) {
+    initZoom = 1;
+    zoomLevelPerPixel = 0.0012;
+    initWidth = 640;
+    calcZoom(initZoom, initWidth, currentWidth, zoomLevelPerPixel);
+  }
+  if (currentWidth <= 639) {
+    initZoom = 1.5;
+    zoomLevelPerPixel = 0.002;
+    initWidth = 639;
+    calcZoom(initZoom, initWidth, currentWidth, zoomLevelPerPixel);
+  }
+}
+function calcZoom(initZoom, initWidth, currentWidth, zoomLevelPerPixel) {
+  var zoomLevel = initZoom - (initWidth - currentWidth) * zoomLevelPerPixel;
+  wrapperZoom.style.zoom = zoomLevel;
+}
+window.addEventListener('resize', reportWindowSize);
+document.addEventListener('DOMContentLoaded', reportWindowSize);
--- src/assets/js/components/common.js
+++ src/assets/js/components/common.js
@@ -1,1 +1,41 @@
+const wrapperZoom = document.querySelector('.wrapperZoom');
+let initZoom = 1;
+
+function reportWindowSize() {
+    const currentWidth = window.innerWidth;
+    let initWidth = 0;
+    let zoomLevelPerPixel = 0;
+    let zoomLevel = 0;
+
+    if (currentWidth > 1399) {
+        initWidth = 1440;
+        zoomLevelPerPixel = 0.0005;
+        calcZoom(initZoom, initWidth, currentWidth, zoomLevelPerPixel)
+    }
+    if (currentWidth <= 1399 && currentWidth >= 961) {
+        zoomLevelPerPixel = 0.0007;
+        initWidth = 1440;
+        calcZoom(initZoom, initWidth, currentWidth, zoomLevelPerPixel)
+    }
+    if (currentWidth <= 960 && currentWidth > 639) {
+        initZoom = 1;
+        zoomLevelPerPixel = 0.0012;
+        initWidth = 640;
+        calcZoom(initZoom, initWidth, currentWidth, zoomLevelPerPixel)
+    }
+    if (currentWidth <= 639) {
+        initZoom = 1.5;
+        zoomLevelPerPixel = 0.002;
+        initWidth = 639;
+        calcZoom(initZoom, initWidth, currentWidth, zoomLevelPerPixel)
+    }
+}
+
+function calcZoom(initZoom, initWidth, currentWidth, zoomLevelPerPixel) {
+    let zoomLevel = initZoom - ((initWidth - currentWidth) * zoomLevelPerPixel);
+    wrapperZoom.style.zoom = zoomLevel;
+}
+
+window.addEventListener('resize', reportWindowSize)
+document.addEventListener('DOMContentLoaded', reportWindowSize)
 
