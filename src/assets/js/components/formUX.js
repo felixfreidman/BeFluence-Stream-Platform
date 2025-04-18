66552f7f06a0e90529ce0e6c36a401e68e91acb1
Felix Freidman <felixfreidman@gmail.com>
2025-04-18 15:17:04 +0300

    Added: page for pagination page for search page for categories Added minor UX fixes like Autocopy values on click with notifications Notification component Auto Spacing for big numbers in average View Data Auto dollar assign for integration price

---
+++ src/assets/js/components/formUX.js
@@ -0,0 +1,44 @@
+if (document.querySelector('main').classList.contains('editPage')) {
+    const editDataForm = document.querySelector('.editContainer__form');
+    const allPersonalInputs = editDataForm.querySelectorAll('input');
+    allPersonalInputs.forEach(input => {
+        console.log(input);
+
+        input.addEventListener('click', () => { copyToClipboard(input) })
+    })
+}
+
+function copyToClipboard(input) {
+    if (input.classList.contains('showModeActive')) {
+        input.select();
+        input.setSelectionRange(0, 99999);
+        navigator.clipboard.writeText(input.value);
+        showNotification('copy');
+    }
+}
+
+const notificationTypes = [
+    {
+        value: 'Скопировано значение!'
+    },
+]
+
+function showNotification(type) {
+    const notificationHolder = document.querySelector('.notifcation');
+    switch (type) {
+        case 'copy':
+            const notificationContainer = document.createElement('div');
+            notificationContainer.classList.add('notifcation__container', 'jsHiddenRightTransformX')
+            notificationContainer.innerHTML = `<span class="notifcation__value">${notificationTypes[0].value}</span>`
+            notificationHolder.appendChild(notificationContainer)
+            setTimeout(() => {
+                notificationContainer.classList.remove('jsHiddenRightTransformX')
+            }, 100)
+            setTimeout(() => {
+                notificationContainer.classList.add('jsHiddenRightTransformX')
+                setTimeout(() => { notificationHolder.removeChild(notificationContainer) }, 500)
+            }, 2000)
+
+            break;
+    }
+}
